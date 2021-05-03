2d7b7585e9e5664a4385420e55ce416b62df2680
Andreas Johan Holme Ulvig <ajulvi18@uia.no>
2021-05-03 12:12:08 +0200

    added start menu

---
+++ app/src/main/java/com/example/knotsandcrosses/GamePlay.kt
@@ -0,0 +1,11 @@
+package com.example.knotsandcrosses
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GamePlay : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_play)
+    }
+}
--- app/src/main/java/com/example/knotsandcrosses/MainActivity.kt
+++ app/src/main/java/com/example/knotsandcrosses/MainActivity.kt
@@ -1,17 +1,29 @@
 package com.example.knotsandcrosses
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import com.example.knotsandcrosses.data.game
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var playerName: String
+    private lateinit var enteredName: TextInputEditText
+    private lateinit var createGameButton: Button
+    private lateinit var joinGameButton: Button
+    private lateinit var joinCodeEntry: TextInputEditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val player = "Stuff"
+        setContentView(R.layout.enter_name)
+        val enterButton: Button = findViewById(R.id.enter_button)
+        enteredName = findViewById(R.id.enter_name)
+        enterButton.setOnClickListener{
+            onEnterClick()
+        }
         val communications = Communications()
-        communications.startGame(this, player)
         val gameList = communications.getGameLists()
         gameList.observe(this, {
             it?.let{
@@ -20,5 +32,45 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+    }
+
+    private fun onEnterClick() {
+        if (enteredName.text.isNullOrEmpty()) {
+            val myToast = Toast.makeText(applicationContext,"Please enter a player name",Toast.LENGTH_SHORT)
+            myToast.show()
+            return
+        } else {
+            playerName = enteredName.text.toString()
+            setContentView(R.layout.activity_main)
+            joinGameButton = (findViewById(R.id.join_game_button))
+            createGameButton = (findViewById(R.id.create_game_button))
+            joinCodeEntry = (findViewById(R.id.enter_id))
+            createGameButton.setOnClickListener{
+                onCreateGameClick()
+            }
+            joinGameButton.setOnClickListener{
+                onJoinGameClick()
+            }
+
+        }
+    }
+
+    private fun onCreateGameClick() {
+        val intent = Intent(this, GamePlay()::class.java)
+        intent.putExtra("player", playerName)
+        startActivity(intent)
+    }
+    private fun onJoinGameClick() {
+        if (joinCodeEntry.text.isNullOrEmpty()) {
+            val myToast =
+                Toast.makeText(applicationContext, "Please enter a join code", Toast.LENGTH_SHORT)
+            myToast.show()
+            return
+        } else {
+            val intent = Intent(this, GamePlay()::class.java)
+            intent.putExtra("player", playerName)
+            intent.putExtra("joinCode", joinCodeEntry.text)
+            startActivity(intent)
+        }
     }
 }
